9c0ed09449bfe065432da467debd7a1008261266
Herosteve <herosteve198g@gmail.com>
2025-06-16 02:10:48 +0300

    for fucks sakes let me update the project

--- Cubesort/ui.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-
-win = Tk()
-win.title('Cube Block Problem')
-wsize = (400, 750)
-win.size(str(wsize[0])+'x'+str(wsize[1]))
-
-win.mainloop()
--- TextAutoFill/FileManager.py
+++
@@ -1,2 +0,0 @@
-def read(path: str) -> int:
-    return 1
