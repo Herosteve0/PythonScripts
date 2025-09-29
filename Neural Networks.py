c3607fa54b11f07d2a98a2e9014576ee04894376
Herosteve <herosteve198g@gmail.com>
2025-09-29 23:18:53 +0300

    meow

--- Neural Networks.py
+++ Neural Networks.py
@@ -12,15 +12,20 @@ class Neuron:
 
     def Output(self, inp: float) -> float: return self.w * inp + self.b
 
-def Data(inp: float) -> float: return inp/1.5
+    def __str__(self):
+        return f"Weight: {self.w}, Bias: {self.b}, Equation: {self.w}*input + {self.b}"
+
+def Data(inp: float) -> float: return inp/1.5 + 5
 
 n = Neuron(1, 0)
-for i in range(15):
-    inp = random.randint(1, 20)
+for i in range(1000000):
+    inp = random.randint(1, 1)
     data = Data(inp)
     prediction = n.Output(inp)
     error = prediction - data
     n.AdjustWeightBy(-error * 0.1)
+    n.AdjustBiasBy(-error * 0.5)
 
 print(Data(100))
 print(n.Output(100))
+print(n)
---
+++ Vector3.py
@@ -0,0 +1,82 @@
+from math import sqrt
+
+class Vector3:
+    def __init__(self, x = None, y: float = None, z: float = None):
+        self.set(x, y, z)
+
+    def set(self, x, y: float = None, z: float = None) -> None:
+        r = [0, 0, 0]
+        try:
+            for i in range(len(x)):
+                r[i] = x[i]
+                if i >= 2:
+                    break
+        except:
+            if x: r[0] = x
+            if y: r[1] = y
+            if z: r[2] = z
+
+        self.x = r[0]
+        self.y = r[1]
+        self.z = r[2]
+
+    def get(self) -> tuple[float]:
+        return (self.x, self.y, self.z)
+
+    def length(self) -> float:
+        return sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
+
+    def normalize(self):
+        l = self.length()
+        if l == 0: return self
+        return Vector3(self.get()) / self.length()
+
+    def __add__(self, other):
+        r = list(self.get())
+        if type(other) == type(self):
+            a = other.get()
+        else:
+            a = [other] * 3
+        for i in range(3):
+            r[i] += a[i]
+        return Vector3(r)
+
+    def __sub__(self, other):
+        r = list(self.get())
+        if type(other) == type(self):
+            a = other.get()
+        else:
+            a = [other] * 3
+        for i in range(3):
+            r[i] -= a[i]
+        return Vector3(r)
+
+    def __mul__(self, other):
+        r = list(self.get())
+        if type(other) == type(self):
+            r[0] = self.y * other.z - self.z * other.y
+            r[1] = 0 - (self.x * other.z - self.z * other.x)
+            r[2] = self.x * other.y - self.y * other.x
+        else:
+            for i in range(3):
+                r[i] *= other
+        return Vector3(r)
+
+    def __truediv__(self, other):
+        r = list(self.get())
+        if type(other) == type(self):
+            return Vector3()
+        else:
+            for i in range(3):
+                r[i] /= other
+        return Vector3(r)
+
+    def __pow__(self, power, modulo=None):
+        if type(self) == type(power):
+            return self.x * power.x + self.y * power.y + self.z * power.z
+        return Vector3()
+
+a = Vector3(-2,3,1)
+b = Vector3(2,3,1)
+c = a * b
+print(c.get())
